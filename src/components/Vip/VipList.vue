5ddd2586dc8c0c09afddd301b9b3e625a896ca9e
857742593 <67151626+857742593@users.noreply.github.com>
2020-08-29 16:26:46 +0800

    Revert "会员列表增加了一个按钮"

--- src/components/Vip/VipList.vue
+++ src/components/Vip/VipList.vue
@@ -13,7 +13,6 @@
           <el-input placeholder="会员卡卡号" v-model="search" clearable></el-input>
         </el-form-item>
         <el-button type="primary" style="width: 100px" @click="getVipById()">查询</el-button>
-        <el-button type="primary" style="width: 100px" @click="getAllList()">显示全部</el-button>
       </el-form>
 
       <el-table
@@ -172,7 +171,6 @@
           consumemoney: null//消费金额
         };
       },
-
       /*
         显示所有会员信息
       */
@@ -183,7 +181,6 @@
         }).then(response => {
           this.tableData = response.data
           this.total = this.tableData.length
-          console.log(response.data)
         }).catch(error => {
           console.log(error);
         })
